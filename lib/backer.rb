1d7cb0f77e6b212280330c8368cb6a7ace6180cf
Jamal Rjoul <jamensky1991@gmail.com>
2019-12-22 18:28:58 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,11 +5,11 @@ class Backer
 
   def initialize(name)
     @name = name
-    @backed_projects = []
+    @@backed_projects = []
   end
 
   def back_project(project)
-    @backed_projects << project
+    @@backed_projects << project
     project.add_backer(self)
   end
 
